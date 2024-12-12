c0b0830a1d18832e71b71429d98e451c894e3dd3
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-12-12 16:00:50 +0400

    Initial commit

---
+++ TestTask/TestTask/TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskApp.swift
+//  TestTask
+//
+//  Created by Temur Chitashvili on 12.12.24.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
