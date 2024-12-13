3ecaccada3fba2470f93e8004caa8af6fd9817bd
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-12-13 07:06:17 +0400

    refactor radio button

--- TestTask/TestTask/Source/Common/Components/TTRadioButton.swift
+++ TestTask/TestTask/Source/Common/Components/TTRadioButton.swift
@@ -8,172 +8,80 @@
 import SwiftUI
 
 enum RadioButtonStyle {
-    case smallSecondary
-    case bigSecondary
     case mediumSecondary
     case graySecondary
 }
 
-struct TTSmallSecondaryRadioButton: View {
+struct TTRadioButton: View {
     let radioButtonStyle: RadioButtonStyle
     @State  var isButtonFilled: Bool = false
     
     var body: some View {
-        if isButtonFilled {
-            ZStack {
+        switch radioButtonStyle {
+        case .mediumSecondary:
+            if isButtonFilled {
                 Circle()
+                    .stroke(lineWidth: 5)
                     .foregroundStyle(.mySecondary)
-                    .frame(width: 10, height: 10)
-                    .padding(10)
+                    .frame(width: 14, height: 14)
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            isButtonFilled = false
+                        }
+                    }
+            } else {
+                Circle()
+                    .stroke(lineWidth: 1)
+                    .foregroundStyle(.myGray)
+                    .frame(width: 14, height: 14)
                 
-                Circle()
-                    .stroke(lineWidth: 2)
-                    .foregroundStyle(.mySecondary)
-                    .frame(width: 20, height: 20)
-            }
-            .onTapGesture {
-                withAnimation(.default) {
-                    isButtonFilled = false
-                }
-            }
-        } else {
-            Circle()
-                .stroke(lineWidth: 2)
-                .foregroundStyle(.mySecondary)
-                .frame(width: 20, height: 20)
-            
-                .onTapGesture {
-                    withAnimation(.default) {
-                        isButtonFilled = true
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            isButtonFilled = true
+                        }
                     }
-                }
-        }
-    }
-}
+            }
 
-struct TTBigSecondaryRadioButton: View {
-    let radioButtonStyle: RadioButtonStyle
-    @State  var isButtonFilled: Bool = false
-    
-    var body: some View {
-        if isButtonFilled {
-            ZStack {
+        case .graySecondary:
+            if isButtonFilled {
                 Circle()
+                    .stroke(lineWidth: 5)
                     .foregroundStyle(.mySecondary)
-                    .frame(width: 10, height: 10)
-                    .padding(10)
+                    .frame(width: 14, height: 14)
+                    .padding(17)
+                    .background(
+                        Circle()
+                            .foregroundStyle(.mySecondary.opacity(0.1))
+                            .frame(width: 48, height: 48)
+                    )
                 
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            isButtonFilled = false
+                        }
+                    }
+            } else {
                 Circle()
-                    .stroke(lineWidth: 2)
-                    .foregroundStyle(.mySecondary)
-                    .frame(width: 20, height: 20)
+                    .stroke(lineWidth: 1)
+                    .foregroundStyle(.myGray)
+                    .frame(width: 14, height: 14)
+                    .padding(17)
+                    .background(
+                        Circle()
+                            .foregroundStyle(.mySecondary.opacity(0.1))
+                            .frame(width: 48, height: 48)
+                    )
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            isButtonFilled = true
+                        }
+                    }
             }
-            .padding(14)
-            .background(
-                Circle()
-                    .foregroundStyle(.mySecondary.opacity(0.1))
-                    .frame(width: 48, height: 48)
-            )
-            .onTapGesture {
-                withAnimation(.default) {
-                    isButtonFilled = false
-                }
-            }
-        } else {
-            Circle()
-                .stroke(lineWidth: 2)
-                .foregroundStyle(.mySecondary)
-                .frame(width: 20, height: 20)
-                .padding(14)
-                .background(
-                    Circle()
-                        .foregroundStyle(.mySecondary.opacity(0.1))
-                        .frame(width: 48, height: 48)
-                )
-                .onTapGesture {
-                    withAnimation(.default) {
-                        isButtonFilled = true
-                    }
-                }
-        }
-    }
-    
-}
-
-struct TTMediumSecondaryRadioButton: View {
-    let radioButtonStyle: RadioButtonStyle
-    @State  var isButtonFilled: Bool = false
-    
-    var body: some View {
-        if isButtonFilled {
-            Circle()
-                .stroke(lineWidth: 5)
-                .foregroundStyle(.mySecondary)
-                .frame(width: 14, height: 14)
-                .onTapGesture {
-                    withAnimation(.default) {
-                        isButtonFilled = false
-                    }
-                }
-        } else {
-            Circle()
-                .stroke(lineWidth: 5)
-                .foregroundStyle(.mySecondary)
-                .frame(width: 14, height: 14)
-                .padding(17)
-                .background(
-                    Circle()
-                        .foregroundStyle(.mySecondary.opacity(0.1))
-                        .frame(width: 48, height: 48)
-                )
-                .onTapGesture {
-                    withAnimation(.default) {
-                        isButtonFilled = true
-                    }
-                }
-        }
-    }
-    
-}
-
-struct TTGraySecondaryRadioButton: View {
-    let radioButtonStyle: RadioButtonStyle
-    @State  var isButtonFilled: Bool = false
-    
-    var body: some View {
-        if isButtonFilled {
-            Circle()
-                .stroke(lineWidth: 1)
-                .foregroundStyle(.myGray)
-                .frame(width: 14, height: 14)
-                .onTapGesture {
-                    withAnimation(.default) {
-                        isButtonFilled = false
-                    }
-                }
-        } else {
-            Circle()
-                .stroke(lineWidth: 1)
-                .foregroundStyle(.myGray)
-                .frame(width: 14, height: 14)
-                .padding(17)
-                .background(
-                    Circle()
-                        .foregroundStyle(.mySecondary.opacity(0.1))
-                        .frame(width: 48, height: 48)
-                )
-                .onTapGesture {
-                    withAnimation(.default) {
-                        isButtonFilled = true
-                    }
-                }
         }
     }
 }
 
 #Preview {
-    TTSmallSecondaryRadioButton(radioButtonStyle: .smallSecondary)
-    TTBigSecondaryRadioButton(radioButtonStyle: .bigSecondary)
-    TTMediumSecondaryRadioButton(radioButtonStyle: .mediumSecondary)
-    TTGraySecondaryRadioButton(radioButtonStyle: .graySecondary)
+    TTRadioButton(radioButtonStyle: .mediumSecondary)
+    TTRadioButton(radioButtonStyle: .graySecondary)
 }
---
+++ TestTask/TestTask/Source/Common/Components/TTTabBar.swift
@@ -0,0 +1,19 @@
+//
+//  TTTabBar.swift
+//  TestTask
+//
+//  Created by Temur Chitashvili on 13.12.24.
+//
+
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+
+#Preview {
+    CustomTabBar()
+}
